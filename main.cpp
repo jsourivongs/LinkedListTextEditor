cd02cf27f2857f2d4dc5f37897c48ed018692bf9
Julian Sourivongs <juliansourivongs@Julians-MacBook-Pro.local>
2018-09-10 00:14:11 -0400

    skeleton flow of program working

--- main.cpp
+++ main.cpp
@@ -6,13 +6,45 @@
 using namespace std;
 int main()
 {
-    string s = "this is a string";
-    istringstream iss(s);
+    //initilize the linked list here
+    //CODE
+
+    //String will hold command
+    string command;
+
     do
     {
-        string subs;
-        iss >> subs;
-        cout << "Substring: " << subs << endl;
-    } while (iss);
+        //take the command
+        cin >> command;
+
+        //commands
+        if (command == "insertEnd"){
+            cout << "you chose insertEnd\n";
+            continue;
+        }
+        if (command == "insert"){
+            cout << "you chose insert\n";
+            continue;
+        }
+        if (command == "delete"){
+            cout << "you chose delete\n";
+            continue;
+        }
+        if (command == "edit"){
+            cout << "you chose edit\n";
+            continue;
+        }
+        if (command == "print"){
+            cout << "you chose print\n";
+            continue;
+        }
+        if (command == "search"){
+            cout << "you chose search\n";
+            continue;
+        }
+
+        //command was not found
+        cout << "bad command\n";
+    } while (command != "quit");
     return 0;
 }
